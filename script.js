d09900def4ffdd8a472d56a59765193781951cee
Afifullah Ajuna Putra <afifullahajuna@gmail.com>
2026-04-08 14:48:08 +0700

    Menambahkan fitur logic todo (add, delete, toggle)

---
+++ script.js
@@ -0,0 +1,24 @@
+function addTask() {
+  const input = document.getElementById("taskInput");
+  const taskText = input.value;
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+
+  li.innerHTML = `
+    <span onclick="toggleDone(this)">${taskText}</span>
+    <button onclick="deleteTask(this)">X</button>
+  `;
+
+  document.getElementById("taskList").appendChild(li);
+  input.value = "";
+}
+
+function deleteTask(btn) {
+  btn.parentElement.remove();
+}
+
+function toggleDone(el) {
+  el.classList.toggle("done");
+}
